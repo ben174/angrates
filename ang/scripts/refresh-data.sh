faba531adafe418eef435d1d513f0e6e3b89f6af
Ben Friedland <ben174@gmail.com>
2014-12-08 16:14:59 -0800

    Removing old v1.0 stuff.

--- ang/scripts/refresh-data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm data/ang2011.xml
-#wget http://www.talk910.com/podcast/ang2011.xml 
-wget http://www.kste.com/podcast/armandgettypodcast.xml
-mv ang2011.xml data/
-rm episodes.db 
-cp ../episodes.db . 
-python provision-data.py
-cp episodes.db ../
