31e88ee7bd44e1b46c27906e600669114f311750
Ben Friedland <ben174@gmail.com>
2014-12-09 17:00:38 -0800

    Trying whitenoise.

--- ang/wsgi.py
+++ ang/wsgi.py
@@ -13,5 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ang.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
