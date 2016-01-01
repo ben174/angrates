5f358d39663eb425936fe3e92a49a7f825b97838
Ben Friedland <ben174@gmail.com>
2015-12-31 18:31:16 -0800

    Basic templates

--- angrates/urls.py
+++ angrates/urls.py
@@ -1,22 +1,14 @@
-"""angrates URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Import the include() function: from django.conf.urls import url, include
-    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import url
-from django.contrib import admin
+
+from podcasts.views import HourMonthArchiveView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Example: /2012/aug/
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[-\w]+)/$',
+        HourMonthArchiveView.as_view(),
+        name="archive_month"),
+    # Example: /2012/08/
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$',
+        HourMonthArchiveView.as_view(month_format='%m'),
+        name="archive_month_numeric"),
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.dates import MonthArchiveView
 
-# Create your views here.
+from podcasts.models import Hour
+
+class HourMonthArchiveView(MonthArchiveView):
+    queryset = Hour.objects.all()
+    date_field = "pub_date"
+    allow_future = True
+    month_format = '%m'
+
