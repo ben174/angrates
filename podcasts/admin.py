d05f7887ffe6bf013218c7f8dcf700a4e94e0cad
Ben Friedland <bfriedland@Prizzod.local>
2016-12-15 14:48:14 -0800

    Add links

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from podcasts.models import Hour, Clip
+from podcasts.models import Hour, Clip, AirDate
 
-admin.site.register([Hour, Clip])
+admin.site.register([Hour, Clip, AirDate])
--- podcasts/util/reddit.py
+++ podcasts/util/reddit.py
@@ -23,38 +23,47 @@ class Reddit:
 
     def get_body(self):
         body = 'Topics include:\n\n'
-        for hour in self.airdate.get_hours():
-            body += '**{}:00 - {}**\n\n{}\n\n\n\n'.format(hour.pub_date.hour, hour.title, hour.description)
+        for index, hour in enumerate(self.airdate.get_hours()):
+            url = 'http://www.armstrongandgettybingo.com/{}/{}/{}/{}/?play={}'.format(
+                hour.feed,
+                hour.pub_date.year,
+                hour.pub_date.month,
+                hour.pub_date.day,
+                index,
+            )
+            body += '**{}:00 - [{}]({})**\n\n{}\n\n\n\n'.format(
+                hour.pub_date.hour,
+                hour.title,
+                url,
+                hour.description,
+            )
 
         footer = ("I'm a freaky-deeky bot. I'll show you SOURCELINK my source if you "
                   "show me yours. I'm made by /u/ben174 @ http://www.bugben.com and my home is "
                   "http://www.armstrongandgettybingo.com - a searchable site full of Armstrong and Getty "
                   "podcasts and clips and other goodness dating back to 2001.")
 
+
         # superscript it
         footer = ' ^'.join(footer.split(' '))
         footer.replace('SOURCELINK', '[^my ^source](https://github.com/ben174/angrates)')
-
         body += '________________________________\n\n^' + footer
+        print body
         return body
 
     def create_post(self):
-        title = '[AnG] Daily Discussion Thread for {}/{}/{} {}'.format(
+        title = '[AnG] Daily Discussion Thread for {}/{}/{}'.format(
             self.airdate.pub_date.month,
             self.airdate.pub_date.day,
             self.airdate.pub_date.year,
-            random.randrange(1000, 9999)
         )
 
         body = self.get_body()
-
         subreddit = 'ArmstrongAndGetty'
-
         submission = self.reddit.subreddit(subreddit).submit(title, selftext=body)
         self.airdate.reddit_post_id = submission.id
         self.airdate.reddit_url = submission.url
         self.airdate.save()
-
 
     def update_post(self):
         submission = self.reddit.submission(self.airdate.reddit_post_id)
